9a2cc3c5356d9a3d017d0257368b54e9714d9f5d
Krabbens <s188660@student.pg.edu.pl>
2023-12-24 09:36:35 +0100

    Add UI class and color palette and spacing modules

---
+++ qui/spacing.py
@@ -0,0 +1,14 @@
+'''module for spacing'''
+
+spacing = {
+    "unit-0": "0px", "unit-1": "4px", "unit-2": "8px", "unit-3": "12px", "unit-3_5": "14px",
+    "unit-4": "16px", "unit-5": "20px", "unit-6": "24px", "unit-7": "28px", "unit-8": "32px",
+    "unit-9": "36px", "unit-10": "40px", "unit-11": "44px", "unit-12": "48px", "unit-13": "52px",
+    "unit-14": "56px", "unit-15": "60px", "unit-16": "64px", "unit-17": "68px", "unit-18": "72px",
+    "unit-20": "80px", "unit-24": "96px", "unit-28": "112px", "unit-32": "128px", "unit-36": "144px",
+    "unit-40": "160px", "unit-44": "176px", "unit-48": "192px", "unit-52": "208px", "unit-56": "224px",
+    "unit-60": "240px", "unit-64": "256px", "unit-72": "288px", "unit-80": "320px", "unit-96": "384px",
+    "unit-xs": "8px", "unit-sm": "12px", "unit-md": "16px", "unit-lg": "22px", "unit-xl": "36px",
+    "unit-2xl": "48px", "unit-3xl": "80px", "unit-4xl": "120px", "unit-5xl": "224px",
+    "unit-6xl": "288", "unit-7xl": "384px", "unit-8xl": "512px", "unit-9xl": "640px"
+}
---
+++ qui/ui.py
@@ -0,0 +1,21 @@
+from .palette import palette
+from .spacing import spacing
+
+from qqt import qqtDictModel
+
+class UI(qqtDictModel):
+    '''
+    This class contains all the UI elements.
+    '''
+
+    def __init__(self):
+        self.ui = {}
+        self.palette = palette
+        self.spacing = spacing
+        
+        self.ui.update(self.palette)
+        self.ui.update(self.spacing)
+        print(self.ui)
+
+        super().__init__("ui", list(self.ui.keys()))
+        self.set_dict(self.ui)
